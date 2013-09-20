e3f73fc39f66106cb4e4aa3b81c41afb13fe8157
HarryHuang1990 <huangfuyang900204@gmail.com>
2013-09-20 21:48:30 +0800

    Berkeley db environment and stores management module has been finished.

---
+++ src/cn/iscas/idse/storage/DBManager.java
@@ -0,0 +1,131 @@
+package cn.iscas.idse.storage;
+
+import java.io.File;
+
+import com.sleepycat.bind.serial.StoredClassCatalog;
+import com.sleepycat.bind.tuple.TupleBinding;
+import com.sleepycat.je.Database;
+import com.sleepycat.je.DatabaseConfig;
+import com.sleepycat.je.DatabaseException;
+import com.sleepycat.je.Environment;
+import com.sleepycat.je.EnvironmentConfig;
+import com.sleepycat.je.SecondaryConfig;
+import com.sleepycat.je.SecondaryDatabase;
+import com.sleepycat.persist.EntityStore;
+import com.sleepycat.persist.StoreConfig;
+
+import cn.iscas.idse.config.SystemConfiguration;
+
+
+
+/**
+ * manager the Berkeley DB using to store index and other data related with the files
+ * @author Harry Huang
+ *
+ */
+public class DBManager {
+	
+	private Environment dbEnvironment;
+	private EntityStore fileLoacationStore;
+	private EntityStore dictionaryStore;
+	private EntityStore indexStore;
+	
+	public DBManager() {}
+	
+	public void setup() {
+		boolean readOnly = true;
+		File environmentHome = new File(SystemConfiguration.rootPath + "/database");
+		try{
+			 
+			// Instantiate an environment configuration object
+			EnvironmentConfig dbEnvConfig = new EnvironmentConfig();
+			StoreConfig storeConfig = new StoreConfig();
+			// Configure the environment for the read-only state as identified
+			// by the readOnly parameter on this method call.
+			dbEnvConfig.setReadOnly(!readOnly);
+			storeConfig.setReadOnly(!readOnly);
+			// If the environment is opened for write, then we want to be 
+			// able to create the environment if it does not exist.
+			dbEnvConfig.setAllowCreate(readOnly);
+			storeConfig.setAllowCreate(readOnly);
+			// Instantiate the Environment and stores. This opens them and also possibly
+			// creates them.
+			this.dbEnvironment = new Environment(environmentHome, dbEnvConfig);
+			this.fileLoacationStore = new EntityStore(this.dbEnvironment, "FileLoacation", storeConfig);
+			this.dictionaryStore = new EntityStore(this.dbEnvironment, "Dictionary", storeConfig);
+			this.indexStore = new EntityStore(this.dbEnvironment, "Index", storeConfig);
+			
+		} catch(DatabaseException dbe) {
+			System.err.println("Error opening environment and store: " + dbe.toString());
+			System.exit(-1);
+		}
+	}
+
+	/**
+	 *  Getter methods: get db environment
+	 * @return
+	 */
+	public Environment getDbEnvironment() {
+		return dbEnvironment;
+	}
+
+	public EntityStore getFileLoacationStore() {
+		return fileLoacationStore;
+	}
+
+	public EntityStore getDictionaryStore() {
+		return dictionaryStore;
+	}
+
+	public EntityStore getIndexStore() {
+		return indexStore;
+	}
+
+	/**
+	 *  Close the environment and stores
+	 *  stores must be closed first, then environment.
+	 */
+	public void close() {
+		if (this.fileLoacationStore != null) {
+			try {
+				this.fileLoacationStore.close();
+			} catch(DatabaseException dbe) {
+				System.err.println("Error closing store fileLoacationStore: " +
+						dbe.toString());
+				System.exit(-1);
+			}
+		}
+		if (this.dictionaryStore != null) {
+			try {
+				this.dictionaryStore.close();
+			} catch(DatabaseException dbe) {
+				System.err.println("Error closing store dictionaryStore: " +
+						dbe.toString());
+				System.exit(-1);
+			}
+		}
+		if (this.indexStore != null) {
+			try {
+				this.indexStore.close();
+			} catch(DatabaseException dbe) {
+				System.err.println("Error closing store indexStore: " +
+						dbe.toString());
+				System.exit(-1);
+			}
+		}
+		
+		if (this.dbEnvironment != null) {
+			try {
+				this.dbEnvironment.close();
+			} catch(DatabaseException dbe) {
+				System.err.println("Error closing environment" + dbe.toString());
+			}
+		}
+	}
+	
+	public static void main(String[]args){
+		DBManager dbm = new DBManager();
+		dbm.setup();
+		dbm.close();
+	}
+}
